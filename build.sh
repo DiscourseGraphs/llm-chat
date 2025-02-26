b52433330e6f3116b81bded012936859f28ab046
sid597 <sid597athens@gmail.com>
2025-02-26 12:16:42 +0530

    testing deploy with build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+npm i -g yarn
+curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
+chmod +x linux-install.sh
+./linux-install.sh
+export PATH=/tmp/clojure/bin:$PATH
+yarn install
+npx shadow-cljs release app
